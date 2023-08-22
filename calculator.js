09a233b1eb586e8ae08ed648645c28237d1a94be
tynou <2sdaiwxet@gmail.com>
2023-08-22 13:34:12 +0500

    Rewritten number sign logic

--- calculator.js
+++ calculator.js
@@ -12,65 +12,54 @@ const operationBtns = document.querySelectorAll(".operation");
 
 let writeNewNumber = true;
 let memory = "0";
-let memorySign = 1;
 let operand = "0";
-let operandSign = 1;
 let operation = null;
 
 numberBtns.forEach((btn) => {
-    btn.onmousedown = (e) => {
-        inputNumber(btn.dataset.value);
-    }
+    btn.onmousedown = (e) => inputNumber(btn.dataset.value);
 });
 
 operationBtns.forEach((btn) => {
-    btn.onmousedown = (e) => {
-        inputOperation(btn.dataset.operation);
-    }
+    btn.onmousedown = (e) => inputOperation(btn.dataset.operation);
 });
 
-clearAllBtn.onmousedown = (e) => {
-    resetLogic();
-    resetDisplay();
-};
+clearAllBtn.onmousedown = (e) => {resetLogic(); resetDisplay()};
 
 clearCurrentBtn.onmousedown = (e) => {
-    if (!writeNewNumber) resetLogic(); resetDisplay();
+    if (!writeNewNumber) {resetLogic(); resetDisplay()};
 
     operand = "0";
 
-    currentInput.innerHTML = getSigned(operand, operandSign);
+    currentInput.innerHTML = operand;
 };
 
 eraseBtn.onmousedown = (e) => {
-    if (!writeNewNumber) resetLogic(); resetDisplay();
+    if (!writeNewNumber) {resetLogic(); resetDisplay()};
 
-    if (operand.length === 1) {
-        operand = "0"
-        operandSign = 1;
+    const hasSign = operand.toString().includes("-");
+    if (operand.length === 1 + (hasSign ? 1 : 0)) {
+        operand = "0";
     } else {
         operand = operand.toString().slice(0,operand.toString().length-1);
     }
-    currentInput.innerHTML = getSigned(operand, operandSign);
+    currentInput.innerHTML = operand;
 };
 
 negateBtn.onmousedown = (e) => {
     if (operand === "0") return;
 
-    operandSign = operandSign === -1 ? 1 : -1;
-    currentInput.innerHTML = getSigned(operand, operandSign);
+    operand = operand * -1;
+    currentInput.innerHTML = operand;
 };
 
 decimalBtn.onmousedown = (e) => {
     if (operand.toString().includes(".")) return;
 
     operand += ".";
-    currentInput.innerHTML = getSigned(operand, operandSign, false);
+    currentInput.innerHTML = operand;
 };
 
-equalBtn.onmousedown = (e) => {
-    operate();
-};
+equalBtn.onmousedown = (e) => operate();
 
 const roundNumber = (number, decPlaces) => Math.round(number * 10**decPlaces) / 10**decPlaces;
 
@@ -78,70 +67,60 @@ const getSigned = (number, sign, round=true) => (sign === -1 ? "-" : "") + (roun
 
 const resetLogic = () => {
     memory = "0";
-    memorySign = 1;
     operand = "0";
-    operandSign = 1;
     operation = null;
     writeNewNumber = true;
 }
 
 const resetDisplay = () => {
     lastOperation.innerHTML = "";
-    currentInput.innerHTML = getSigned(operand, operandSign);
+    currentInput.innerHTML = operand;
 }
 
 const inputNumber = (number) => {
-    if (getSigned(operand, operandSign, false).length >= 16) return;
+    if (operand.length >= 16) return;
     if (!writeNewNumber) {resetLogic(); resetDisplay()};
 
     operand = operand === "0" ? number.toString() : operand + number;
-    currentInput.innerHTML = getSigned(operand, operandSign, false);
+    currentInput.innerHTML = operand;
 };
 
 const inputOperation = (newOperation) => {
     memory = operand;
-    memorySign = operandSign
     operand = "0";
-    operandSign = 1;
     operation = newOperation;
     writeNewNumber = true;
 
-    lastOperation.innerHTML = `${getSigned(memory, memorySign)} ${operation}`;
+    lastOperation.innerHTML = `${memory} ${operation}`;
 };
 
 const operate = () => {
-    let signedMemory = +getSigned(memory, memorySign);
-    let signedOperand = +getSigned(operand, operandSign);
-
-    lastOperation.innerHTML = operation ? `${signedMemory} ${operation} ${signedOperand} = ` : `${signedOperand} = `;
+    lastOperation.innerHTML = operation ? `${+memory} ${operation} ${+operand} = ` : `${+operand} = `;
 
     switch (operation) {
         case "/":
-            if (signedOperand === 0) {
+            if (+operand === 0) {
                 currentInput.innerHTML = "err";
                 resetLogic();
                 return;
             }
-            operand = signedMemory / signedOperand;
+            operand = +memory / +operand;
             break;
         case "*":
-            operand = signedMemory * signedOperand;
+            operand = +memory * +operand;
             break;
         case "-":
-            operand = signedMemory - signedOperand;
+            operand = +memory - +operand;
             break;
         case "+":
-            operand = signedMemory + signedOperand;
+            operand = +memory + +operand;
             break;
     }
-    operandSign = operand < 0 ? -1 : 1;
-    /* operand = getSigned(operand, operandSign); */
-    operand *= operandSign;
 
     operation = null;
     writeNewNumber = false;
     
-    currentInput.innerHTML = getSigned(operand, operandSign);
+    currentInput.innerHTML = operand;
 };
 
 window.onload = () => {resetLogic(); resetDisplay()};
